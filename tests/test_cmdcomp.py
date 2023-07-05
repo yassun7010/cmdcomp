9a068dd208cee59cd3d3004d556824612593bf62
Yasutanium <yassun4dev@outlook.com>
2023-07-06 00:48:16 +0900

    Add: yaml test case.

--- tests/test_cmdcomp.py
+++ tests/test_cmdcomp.py
@@ -2,13 +2,12 @@ from pathlib import Path
 
 from pytest import CaptureFixture
 
-from cmdcomp import __version__
 from cmdcomp.app import App
 
 SAMPLES_DIR = Path(__file__).parent.parent / "samples"
 
 
-def test_bash_sample(capsys: CaptureFixture) -> None:
+def test_sample_toml_bash(capsys: CaptureFixture) -> None:
     App.run(
         [
             "--file",
@@ -21,11 +20,37 @@ def test_bash_sample(capsys: CaptureFixture) -> None:
     assert capsys.readouterr().out == open(SAMPLES_DIR / "output.bash").read()
 
 
-def test_zsh_sample(capsys: CaptureFixture) -> None:
+def test_sample_toml_zsh(capsys: CaptureFixture) -> None:
     App.run(
         [
             "--file",
             str(SAMPLES_DIR / "config.cmdcomp.toml"),
+            "--shell-type",
+            "zsh",
+        ]
+    )
+
+    assert capsys.readouterr().out == open(SAMPLES_DIR / "output.zsh").read()
+
+
+def test_sample_yaml_bash(capsys: CaptureFixture) -> None:
+    App.run(
+        [
+            "--file",
+            str(SAMPLES_DIR / "config.cmdcomp.yaml"),
+            "--shell-type",
+            "bash",
+        ]
+    )
+
+    assert capsys.readouterr().out == open(SAMPLES_DIR / "output.bash").read()
+
+
+def test_sample_yaml_zsh(capsys: CaptureFixture) -> None:
+    App.run(
+        [
+            "--file",
+            str(SAMPLES_DIR / "config.cmdcomp.yaml"),
             "--shell-type",
             "zsh",
         ]
