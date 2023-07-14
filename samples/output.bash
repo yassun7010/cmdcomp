a6c68b1f6fd2c7d152a53ccfdc8473b3775fb975
Yasutanium <yassun4dev@outlook.com>
2023-07-14 23:49:36 +0900

    Update: template.

--- samples/output.bash
+++ samples/output.bash
@@ -6,28 +6,38 @@ _your_cli_command_name() {
 
     case $cword in
         1)
-            COMPREPLY=( $(compgen -W "-h --help --version welcome list ls execute restart shell log cd test and_normal_options_work" -- "$cur") ) ;;
+            COMPREPLY=( $(compgen -W "-h --help --version welcome list ls execute restart shell log cd test and_normal_options_work" -- "$cur") )
+            ;;
         2)
             case ${words[1]} in
                 list|ls)
-                    COMPREPLY=( $(compgen -W "-a" -- "$cur") ) ;;
+                    COMPREPLY=( $(compgen -W "-a" -- "$cur") )
+                    ;;
                 execute|restart|shell|log)
-                    COMPREPLY=( $(compgen -W "$(your_app_name ps -s)" -- "$cur") ) ;;
+                    COMPREPLY=( $(compgen -W "$(your_app_name ps -s)" -- "$cur") )
+                    ;;
                 cd)
-                    file_completion "$(cd $(dirname $0); pwd)/../apps" ;;
+                    file_completion "$(cd $(dirname $0); pwd)/../apps"
+                    ;;
                 test)
-                    COMPREPLY=( $(compgen -W "rubocop" -- "$cur") ) ;;
+                    COMPREPLY=( $(compgen -W "rubocop" -- "$cur") )
+                    ;;
                 and_normal_options_work)
-                    COMPREPLY=( $(compgen -W "-h --help foo bar" -- "$cur") ) ;;
-            esac ;;
+                    COMPREPLY=( $(compgen -W "-h --help foo bar" -- "$cur") )
+                    ;;
+            esac
+            ;;
         3)
             case ${words[1]} in
                 test)
                     case ${words[2]} in
                         rubocop)
-                            COMPREPLY=( $(compgen -W "-A" -- "$cur") ) ;;
-                    esac ;;
-            esac ;;
+                            COMPREPLY=( $(compgen -W "-A" -- "$cur") )
+                            ;;
+                    esac
+                    ;;
+            esac
+            ;;
     esac
 }
 
--- samples/output.zsh
+++ samples/output.zsh
@@ -5,28 +5,38 @@ _your_cli_command_name() {
 
     case "$(($CURRENT-1))" in
         1)
-            _values 'your_cli_command_name' -h --help --version welcome list ls execute restart shell log cd test and_normal_options_work ;;
+            _values 'your_cli_command_name' -h --help --version welcome list ls execute restart shell log cd test and_normal_options_work
+            ;;
         2)
             case ${words[1]} in
                 list|ls)
-                    _values 'list|ls' -a ;;
+                    _values 'list|ls' -a
+                    ;;
                 execute|restart|shell|log)
-                    _values 'execute|restart|shell|log' $(your_app_name ps -s) ;;
+                    _values 'execute|restart|shell|log' $(your_app_name ps -s)
+                    ;;
                 cd)
-                    _files -W "$(cd $(dirname $0); pwd)/../apps" ;;
+                    _files -W "$(cd $(dirname $0); pwd)/../apps"
+                    ;;
                 test)
-                    _values 'test' rubocop ;;
+                    _values 'test' rubocop
+                    ;;
                 and_normal_options_work)
-                    _values 'and_normal_options_work' -h --help foo bar ;;
-            esac ;;
+                    _values 'and_normal_options_work' -h --help foo bar
+                    ;;
+            esac
+            ;;
         3)
             case ${words[1]} in
                 test)
                     case ${words[2]} in
                         rubocop)
-                            _values 'rubocop' -A ;;
-                    esac ;;
-            esac ;;
+                            _values 'rubocop' -A
+                            ;;
+                    esac
+                    ;;
+            esac
+            ;;
     esac
 }
 
