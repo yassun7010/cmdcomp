e7a27a1bc9205b26f86e02cd41c4b733a1b12ace
Yasutanium <yassun4dev@outlook.com>
2023-07-16 23:26:12 +0900

    Update: Improved scalability.

--- cmdcomp/completion.py
+++ cmdcomp/completion.py
@@ -1,7 +1,10 @@
 from cmdcomp.config import Config
 from cmdcomp.shell import ShellType
 from cmdcomp.v1_config.completion import generate_v1
+from cmdcomp.v1_config.v1_config import V1Config
 
 
-def generate(shell: ShellType, config: Config):
-    return generate_v1(shell, config.root)
+def generate(shell: ShellType, config: Config) -> str:
+    match config.root:
+        case V1Config():
+            return generate_v1(shell, config.root)
