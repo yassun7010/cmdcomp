0cc5f0d2ff31c41abff86a14cae98097946d34f1
yassun4dev <yassun4dev@outlook.com>
2023-07-17 02:55:54 +0900

    Add: generate_v2.

--- cmdcomp/completion.py
+++ cmdcomp/completion.py
@@ -1,4 +1,5 @@
 from cmdcomp.config import Config
+from cmdcomp.exception import NeverReach
 from cmdcomp.shell import ShellType
 from cmdcomp.v1.completion import generate_v1
 from cmdcomp.v1.config import V1Config
@@ -8,3 +9,9 @@ def generate(shell: ShellType, config: Config) -> str:
     match config.root:
         case V1Config():
             return generate_v1(shell, config.root)
+
+        # case V2Config():
+        #     return generate_v2(shell, config.root)
+
+        case _:
+            raise NeverReach(config.root)
---
+++ cmdcomp/v2/completion.py
@@ -0,0 +1,6 @@
+from cmdcomp.shell import ShellType
+from cmdcomp.v2.config import V2Config
+
+
+def generate_v2(shell: ShellType, config: V2Config) -> str:
+    return "Not implemented yet."
