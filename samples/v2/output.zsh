b75e4e3ab63dab3bcd4be04c0855663be449c814
yassun4dev <yassun4dev@outlook.com>
2023-07-23 21:45:36 +0900

    WIP: support subcommand.

--- samples/v2/output.zsh
+++ samples/v2/output.zsh
@@ -4,6 +4,11 @@ _cliname() {
     local context curcontext=$curcontext state line
     declare -A opt_args
     local ret=1
+    local -a _subcmds
+    _subcmds=(
+        'sub1[sub1 description]'
+        'sub2[sub2 description]'
+    )
 
     _arguments -C \
         {--file,-f}'[config filepath.]:file:_files -W "$HOME"' \
@@ -13,10 +18,25 @@ _cliname() {
         --verbose'[verbose description]' \
         --no-verbose'[no verbose description]' \
         --help'' \
-        '*:: :->null' \
-        && return 1
+        1': :_values "subcommand" ${_subcmds[@]}' \
+        '*:: :->args' \
+        && ret=0
 
-    return 0
+
+    case $state in
+        (args)
+            case $words[1] in
+                (sub1)
+                    _arguments '-s[sort output]' '--l[long output]' '-l[long output]' && ret=0
+                    ;;
+                (sub2)
+                    _arguments '-s[sort output]' '--l[long output]' '-l[long output]' && ret=0
+                    ;;
+            esac
+            ;;
+    esac
+
+    return ret
 }
 
 compdef _cliname cliname cliname2
