a0ac6aff15d8024e731e8518de44e59b6be90fd9
yassun4dev <yassun4dev@outlook.com>
2023-07-23 23:38:41 +0900

    Add: newline.

--- samples/v2/output.zsh
+++ samples/v2/output.zsh
@@ -14,6 +14,7 @@ _cliname() {
         cd'[cd project directory.]'
         test'[test command.]'
       )
+
       _arguments -C \
         --verbose'[verbose output.]' \
         --no-verbose'[no verbose output.]' \
@@ -22,6 +23,7 @@ _cliname() {
         1': :_values "subcommand" ${__cliname_subcmds[@]}' \
         '*:: :->args' \
         && ret=0
+
       cmd_name=$words[1]
       case $state in
         args)
@@ -31,21 +33,25 @@ _cliname() {
                 --all'[list all files.]' \
                 && ret=0
               ;;
+
             cd)
               _arguments -C \
                 1':file:_files' \
                 && ret=0
               ;;
+
             test)
               local -a __test_subcmds
               __test_subcmds=(
                 rubocop'[run rubocop.]'
                 pytest'[run pytest.]'
               )
+
               _arguments -C \
                 1': :_values "subcommand" ${__test_subcmds[@]}' \
                 '*:: :->args' \
                 && ret=0
+
               cmd_name=$words[1]
               case $state in
                 args)
@@ -55,18 +61,24 @@ _cliname() {
                         --auto-correct'[auto correct.]' \
                         && ret=0
                       ;;
+
                     pytest)
                       _arguments -C \
                         && ret=0
                       ;;
+
                   esac
                   ;;
+
               esac
               ;;
+
           esac
           ;;
+
       esac
       ;;
+
   esac
 
   return ret
