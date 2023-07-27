97c2b4cf5386a8680a81701345c082c1c1ce35da
yassun4dev <yassun4dev@outlook.com>
2023-07-28 00:51:07 +0900

    WIP: 補完ファイルの骨格を作成。

--- samples/v2/output.bash
+++ samples/v2/output.bash
@@ -1,7 +1,35 @@
 #!/bin/bash
 
 _cliname() {
-  :
+  local i cur prev opts cmd
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  cmd=""
+  opts=""
+
+  for i in ${COMP_WORDS[@]} do
+    case "${cmd},${i}" in
+      ",$1")
+        cmd="cliname"
+        ;;
+
+      cliname,list)
+        cmd="_cliname_list_subcommands"
+        ;;
+
+      cliname,cd)
+        cmd="_cliname_cd_subcommands"
+        ;;
+
+      cliname,test)
+        cmd="_cliname_test_subcommands"
+        ;;
+
+      *)
+        ;;
+    esac
+  done
 }
 
 complete -F _cliname -o bashdefault -o default cliname
