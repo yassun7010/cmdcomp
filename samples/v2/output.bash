b2276026118b79d8f6f5ae719072fd52340a1bc2
yassun4dev <yassun4dev@outlook.com>
2023-07-29 01:28:43 +0900

    WIP: update completion template.

--- samples/v2/output.bash
+++ samples/v2/output.bash
@@ -8,28 +8,90 @@ _cliname() {
   cmd=""
   opts=""
 
-  for i in ${COMP_WORDS[@]} do
+  for i in ${COMP_WORDS[@]}; do
     case "${cmd},${i}" in
       ",$1")
-        cmd="cliname"
+        cmd="_cliname"
         ;;
 
-      cliname,list)
-        cmd="_cliname_list_subcommands"
+      _cliname,list)
+        cmd=""
         ;;
 
-      cliname,cd)
-        cmd="_cliname_cd_subcommands"
+      _cliname,cd)
+        cmd=""
         ;;
 
-      cliname,test)
-        cmd="_cliname_test_subcommands"
+      _cliname,test)
+        cmd=""
+        ;;
+
+      _cliname,rubocop)
+        cmd=""
+        ;;
+
+      _cliname,pytest)
+        cmd=""
         ;;
 
       *)
         ;;
     esac
   done
+
+  case "${cmd}" in
+    cliname)
+      opts="list cd test --verbose --no-verbose --version --help"
+      if [[ ${cur} == -* || ${COMP_CWORD} -eq 1 ]] ; then
+          COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+          return 0
+      fi
+
+      case "${prev}" in
+          --verbose)
+              COMPREPLY=()
+              ;;
+
+          --no-verbose)
+              COMPREPLY=()
+              ;;
+
+          --version)
+              COMPREPLY=()
+              ;;
+
+          --help)
+              COMPREPLY=()
+              ;;
+
+          *)
+              COMPREPLY=()
+              ;;
+
+      esac
+
+      return 0
+      ;;
+
+    _cliname_list_subcommands)
+      opts=""
+
+      COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+      ;;
+
+    _cliname_cd_subcommands)
+      opts=""
+
+      COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+      ;;
+
+    _cliname_test_subcommands)
+      opts="rubocop pytest"
+
+      COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+      ;;
+
+  esac
 }
 
 complete -F _cliname -o bashdefault -o default cliname
