1540d47b5ab5caeb9d72d77b4067803c39661d21
yassun4dev <yassun4dev@outlook.com>
2023-07-31 01:08:34 +0900

    Update: test case.

--- tests/test_cmdcomp.py
+++ tests/test_cmdcomp.py
@@ -7,44 +7,43 @@ from tests.conftest import SAMPLES_DIR
 
 
 @pytest.mark.parametrize("version", ["v1", "v2"])
-@pytest.mark.parametrize("config_format", ["yaml"])
+@pytest.mark.parametrize("config_format", ["yaml", "toml"])
 @pytest.mark.parametrize("shell", ShellType)
-def test_output_file_to_samples(
-    version: str,
-    config_format: str,
-    shell: ShellType,
-) -> None:
-    App.run(
-        [
-            "--file",
-            str(SAMPLES_DIR / version / f"config.cmdcomp.{config_format}"),
-            "--shell-type",
-            shell.value,
-            "--output",
-            str(SAMPLES_DIR / version / f"output.{shell.value}"),
-        ]
-    )
+class TestApplication:
+    def test_application(
+        self,
+        capsys: CaptureFixture,
+        version: str,
+        config_format: str,
+        shell: ShellType,
+    ) -> None:
+        App.run(
+            [
+                "--file",
+                str(SAMPLES_DIR / version / f"config.cmdcomp.{config_format}"),
+                "--shell-type",
+                shell.value,
+            ]
+        )
 
+        assert (
+            capsys.readouterr().out
+            == open(SAMPLES_DIR / version / f"output.{str(shell.value)}").read()
+        )
 
-@pytest.mark.parametrize("version", ["v1", "v2"])
-@pytest.mark.parametrize("config_format", ["yaml"])
-@pytest.mark.parametrize("shell", ShellType)
-def test_application(
-    capsys: CaptureFixture,
-    version: str,
-    config_format: str,
-    shell: ShellType,
-) -> None:
-    App.run(
-        [
-            "--file",
-            str(SAMPLES_DIR / version / f"config.cmdcomp.{config_format}"),
-            "--shell-type",
-            shell.value,
-        ]
-    )
-
-    assert (
-        capsys.readouterr().out
-        == open(SAMPLES_DIR / version / f"output.{str(shell.value)}").read()
-    )
+    def test_output_file_to_samples(
+        self,
+        version: str,
+        config_format: str,
+        shell: ShellType,
+    ) -> None:
+        App.run(
+            [
+                "--file",
+                str(SAMPLES_DIR / version / f"config.cmdcomp.{config_format}"),
+                "--shell-type",
+                shell.value,
+                "--output",
+                str(SAMPLES_DIR / version / f"output.{shell.value}"),
+            ]
+        )
