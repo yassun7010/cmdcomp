1eed2d2094298ef825385e4e3ed35ddfcd62b70e
yasutani_naoto <yasutani_naoto@cyberagent.co.jp>
2023-08-20 01:51:18 +0900

    Fix: select schema.

--- cmdcomp/v2/command/argument/select_argument.py
+++ cmdcomp/v2/command/argument/select_argument.py
@@ -1,12 +1,9 @@
-from collections import OrderedDict
 from functools import cached_property
 from typing import Annotated, Literal
 
 from pydantic import Field
 
-from cmdcomp.exception import NeverReach
 from cmdcomp.model import Model
-from cmdcomp.v2.command.argument.value_argument import V2ValueArgument
 from cmdcomp.v2.mixin.has_alias import HasAlias
 
 
@@ -24,7 +21,7 @@ class V2SelectArgument(HasAlias, Model):
     )
 
     raw_values: Annotated[
-        list[str | V2ValueArgument] | OrderedDict[str, str | V2ValueArgument],
+        str | list[str],
         Field(
             title="candidate selection.",
             alias="values",
@@ -32,22 +29,10 @@ class V2SelectArgument(HasAlias, Model):
     ]
 
     @cached_property
-    def values(self) -> list[V2ValueArgument]:
+    def values(self) -> list[str]:
         match self.raw_values:
             case str():
-                return [V2ValueArgument(value=self.raw_values)]
+                return [self.raw_values]
 
             case list():
-                return [
-                    v if isinstance(v, V2ValueArgument) else V2ValueArgument(value=v)
-                    for v in self.raw_values
-                ]
-
-            case OrderedDict():
-                return [
-                    V2ValueArgument(value=v) if isinstance(v, str) else v
-                    for v in self.raw_values.values()
-                ]
-
-            case _:
-                raise NeverReach(self.raw_values)
+                return self.raw_values
