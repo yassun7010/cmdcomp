61115749649e51f9a2c1adfe1eb4d5ab7a0b3895
yasutani_naoto <yasutani_naoto@cyberagent.co.jp>
2023-08-20 15:36:49 +0900

    Add: doc test.

--- tests/test_doc.py
+++ tests/test_doc.py
@@ -18,7 +18,7 @@ class TestAppHelp:
         with open(DATA_DIR / "help.txt", "w") as file:
             file.write(capsys.readouterr().out)
 
-    def test_v2_config(self):
+    def test_v2_config(self) -> None:
         with open(DATA_DIR / "jinja" / "v2_config_structure.yaml.jinja", "rb") as file:
             config.load(
                 file,
@@ -34,7 +34,7 @@ class TestAppHelp:
                 ),
             )
 
-    def test_v2_positional_arguments_command(self):
+    def test_v2_positional_arguments_command(self) -> None:
         with open(
             DATA_DIR / "jinja" / "v2_positional_arguments_command_structure.yaml.jinja",
             "r",
@@ -49,7 +49,7 @@ class TestAppHelp:
                 ),
             )
 
-    def test_v2_subcommands_command(self):
+    def test_v2_subcommands_command(self) -> None:
         with open(
             DATA_DIR / "jinja" / "v2_subcommands_command_structure.yaml.jinja",
             "r",
@@ -68,3 +68,16 @@ class TestAppHelp:
                     ).model_dump_json()
                 ),
             )
+
+    @pytest.mark.parametrize("config_file", (DATA_DIR / "config").iterdir())
+    def test_config(self, config_file) -> None:
+        with open(config_file, "rb") as file:
+            config.load(file)
+
+    @pytest.mark.parametrize("app_info_file", (DATA_DIR / "v2_app_info").iterdir())
+    def test_v2_app_info(self, app_info_file) -> None:
+        class V2App(model.Model):
+            app: V2AppInfo
+
+        with open(app_info_file, "rb") as file:
+            model.load(V2App, file)
